dab8bed697294a8e11d07b063b4d11ac6402dea4
algoreadme <dhikaaditya1@gmail.com>
2024-12-26 00:12:43 +0700

    initialize project

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,62 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by @algoreadme on 30/11/24.
+//
+
+import XCTest
+import EssentialFeed
+
+final class RemoteFeedLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSutFactory()
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSutFactory(url: url)
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadTwice_requestDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSutFactory(url: url)
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSutFactory()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
+        
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+    
+    // MARK: - Helpers
+    private func makeSutFactory(
+        url: URL = URL(string: "https://a-url.com")!
+    ) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+        var completions = [(Error) -> Void]()
+        
+        func get(from url: URL, completion: @escaping(Error) -> Void) {
+            completions.append(completion)
+            requestedURLs.append(url)
+        }
+    }
+}
