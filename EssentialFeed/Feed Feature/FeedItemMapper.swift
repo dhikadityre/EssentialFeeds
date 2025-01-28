edeefff87cf8c2fa563c240101e98b35797bd780
algoreadme <dhikaaditya1@gmail.com>
2025-01-28 10:50:17 +0700

    feat: Move FeedItmesMapper to its own file

---
+++ EssentialFeed/Feed Feature/FeedItemMapper.swift
@@ -0,0 +1,40 @@
+//
+//  FeedItemMapper.swift
+//  EssentialFeed
+//
+//  Created by DHIKA ADITYA ARE on 28/01/25.
+//
+
+import Foundation
+
+internal final class FeedItemMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL // ubah menjadi `image` seusai response json
+        
+        // create Mapping dengan computed
+        var item: FeedItem {
+            return FeedItem(
+                id: id,
+                description: description,
+                location: location,
+                imageURL: image
+            )
+        }
+    }
+    
+    private static var OK_200: Int { return 200 }
+    
+    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
+    }
+}
