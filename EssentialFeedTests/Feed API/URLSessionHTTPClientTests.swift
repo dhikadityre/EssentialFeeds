7777188f7da4651fff84e24520aa557c35a26116
@algoreadme <dhikaaditya1@gmail.com>
2026-06-01 16:46:13 +0700

    feat: resume DataTask on Creation

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -18,7 +18,7 @@ class URLSessionHTTPClient {
     func get(from url: URL) {
         session.dataTask(with: url) { _, _, _ in
             
-        }
+        }.resume() // tambahkan ini
     }
 }
 
@@ -35,16 +35,43 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedUrls, [url])
     }
     
+    func test_getFromURL_resumDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let task = UrlSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
     // MARK: - Helpers
     private class URLSessionSpy: URLSession {
         var receivedUrls = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
             receivedUrls.append(url)
-            return FakeUrlSessionDataTask() // mereturn disini
+            return stubs[url] ?? FakeUrlSessionDataTask()
         }
+    }
+    
+    private class FakeUrlSessionDataTask: URLSessionDataTask {
+        override func resume() {} // membuat override resume
+    }
+    
+    private class UrlSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount = 0
         
-        private class FakeUrlSessionDataTask: URLSessionDataTask {} // disini dibuat karena kita tidak mau execute network saat melakukan pengujian.
-        
+        override func resume() {
+            resumeCallCount += 1
+        }
     }
 }
