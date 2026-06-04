46831902ccb7723f33997dbe6e2aa7797369951c
@algoreadme <dhikaaditya1@gmail.com>
2026-06-04 13:57:25 +0700

    feat: make anyUrl to shared factory method

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -37,7 +37,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performGETRequestWithURL() {
-        let givenUrl = URL(string: "https://a-url-by-dhika")!
+        let givenUrl = anyURL()
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequest { request in
@@ -53,14 +53,13 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         
-        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error) // penyesuaian berdasarkan table
+        URLProtocolStub.stub(url: anyURL(), data: nil, response: nil, error: error) // penyesuaian berdasarkan table
         
         let exp = expectation(description: "Wait for completion")
         
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL()) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 // XCTAssertEqual(receivedError, error)
@@ -83,6 +82,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
     
     private class URLProtocolStub: URLProtocol {
