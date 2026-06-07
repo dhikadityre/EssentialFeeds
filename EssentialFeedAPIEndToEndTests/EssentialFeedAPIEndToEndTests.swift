e21d33a34afb0f08d942fc138de0a208a1223810
@algoreadme <dhikaaditya1@gmail.com>
2026-06-07 17:46:34 +0700

    feat: Add end-to-end API test in a seperate test target

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,111 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by DHIKA ADITYA ARE on 07/06/26.
+//
+
+import XCTest
+import EssentialFeed
+
+class EssentialFeedAPIEndToEndTests: XCTestCase {
+    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion handler")
+        var receivedResult: LoadFeedResult?
+        loader.load { results in
+            receivedResult = results
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        switch receivedResult {
+        case let .success(items):
+            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
+            
+            /*
+            /// Tidak di rekomendasikan. karena jika salah satu data ada yg salah/berubah sulit untuk di deteksi.
+            items.enumerated().forEach { (index, item) in
+                XCTAssertEqual(item, expectedItems(at: index), "Unexpected item values at index \(index)")
+            }
+            */
+            
+            XCTAssertEqual(items[0], expectedItems(at: 0), "Unexpected item values at index \(0)")
+            XCTAssertEqual(items[1], expectedItems(at: 1), "Unexpected item values at index \(1)")
+            XCTAssertEqual(items[2], expectedItems(at: 2), "Unexpected item values at index \(2)")
+            XCTAssertEqual(items[3], expectedItems(at: 3), "Unexpected item values at index \(3)")
+            XCTAssertEqual(items[4], expectedItems(at: 4), "Unexpected item values at index \(4)")
+            XCTAssertEqual(items[5], expectedItems(at: 5), "Unexpected item values at index \(5)")
+            XCTAssertEqual(items[6], expectedItems(at: 6), "Unexpected item values at index \(6)")
+            XCTAssertEqual(items[7], expectedItems(at: 7), "Unexpected item values at index \(7)")
+        case let .failure(error):
+            XCTFail("Expected successful feed result, got \(error) instead")
+        default:
+            XCTFail("Expected successful feed result, got no result instead")
+        }
+    }
+    
+    private func expectedItems(at index: Int) -> FeedItem {
+        return FeedItem(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            imageURL: imageURL(at: index)
+        )
+    }
+
+    private func id(at index: Int) -> UUID {
+        return [
+            UUID(uuidString: "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6")!,
+            UUID(uuidString: "BA298A85-6275-48D3-8315-9C8F7C1CD109")!,
+            UUID(uuidString: "5A0D45B3-8E26-4385-8C5D-213E160A5E3C")!,
+            UUID(uuidString: "FF0ECFE2-2879-403F-8DBE-A83B4010B340")!,
+            UUID(uuidString: "DC97EF5E-2CC9-4905-A8AD-3C351C311001")!,
+            UUID(uuidString: "557D87F1-25D3-4D77-82E9-364B2ED9CB30")!,
+            UUID(uuidString: "A83284EF-C2DF-415D-AB73-2A9B8B04950B")!,
+            UUID(uuidString: "F79BD7F8-063F-46E2-8147-A67635C3BB01")!
+        ][index]
+    }
+
+    private func description(at index: Int) -> String? {
+        return [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
+    }
+
+    private func location(at index: Int) -> String? {
+        return [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
+    }
+
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: [
+            "https://url-1.com",
+            "https://url-2.com",
+            "https://url-3.com",
+            "https://url-4.com",
+            "https://url-5.com",
+            "https://url-6.com",
+            "https://url-7.com",
+            "https://url-8.com"
+        ][index])!
+    }
+}
