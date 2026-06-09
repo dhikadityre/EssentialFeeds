ecb9be3a6babc6c77a095084d41eb91efa3730c9
@algoreadme <dhikaaditya1@gmail.com>
2026-06-09 16:13:51 +0700

    feat: save command request cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,9 +6,18 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
-    init(store: FeedStore) {}
+    private let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
 }
 
 /// The Feed Store is a helper class representing the framework side
@@ -17,12 +26,38 @@ class LocalFeedLoader {
 /// artinya: Feed Store ini digunakan sebagai contract yg dibutuhkan client tanpa perlu memikirkan akan menggunakan framework nantinya.
 class FeedStore {
     var deletedCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deletedCachedFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteTheCacheUponCreation() {
         let store = FeedStore()
+        /// To decouple the application from framework details, we dont let frameworks dictate the usecase interface (ex: adding codable requirement, or core data managed context parameters).
+        /// We do so by test-driving the interface the use case needs for its collaborator,
+        /// rather then defining the interface upfront to facilitate a spesific framework implementation.
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deletedCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deletedCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helper
+    private func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
