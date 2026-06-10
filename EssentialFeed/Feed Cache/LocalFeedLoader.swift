f54af46aac5c159cf8719a158644e4b2af3a80e6
@algoreadme <dhikaaditya1@gmail.com>
2026-06-10 15:26:01 +0700

    feat: move `LocalFeedLoader` and `FeedStore` colaborator to its own file in production code

---
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,56 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by DHIKA ADITYA ARE on 10/06/26.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        /// Disini kita dapat menjalankan secara sync atau biarkan framework menjalankan secara async
+        /// yang pasti adalah `deleteCachedFeed` harus dijalankan terlebih dahulu
+        //// store.deleteCachedFeed()
+        
+        store.deleteCachedFeed { [weak self] error in
+            guard let self else { return }
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, completion: completion)
+            }
+        }
+    }
+    
+    private func cache(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.insert(
+            items,
+            timestamp: currentDate(),
+            completion: { [weak self] error in
+                guard self != nil else { return }
+                completion(error)
+            }
+        )
+    }
+}
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(
+        _ items: [FeedItem],
+        timestamp: Date,
+        completion: @escaping InsertionCompletion
+    )
+}
