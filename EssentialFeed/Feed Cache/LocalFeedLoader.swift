221d6b563f8594f287a87d3d7dc4f4b7b4be18b5
@algoreadme <dhikaaditya1@gmail.com>
2026-06-14 16:34:29 +0700

    feat: Make `FeedCachePolicy` static since it doesnt keep any state

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -7,28 +7,34 @@
 
 import Foundation
 
+/// The `LocalFeedLoader` should encapsulate application spesific logic only, and communicate with models to perform business logic.
+/// Rules and Policies (ex: validation logic) are better suited in a Domain Model that is Application Agnostic
+/// So it can be reused accross application
+private final class FeedCachePolicy {
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private init() {}
+    
+    private static var maxCacheInDays: Int {
+        return 7
+    }
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard
+            let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheInDays, to: timestamp) // calendar dapet dari variable.
+        else { return false }
+        return date < maxCacheAge
+    }
+}
+
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
-    private let calendar = Calendar(identifier: .gregorian)
-    
-//    public typealias SaveResult = Error?
-//    public typealias RetrieveResult = LoadFeedResult
+    // private let cachePolicy: FeedCachePolicy = FeedCachePolicy()
     
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
-    }
-    
-    private var maxCacheInDays: Int {
-        return 7
-    }
-    
-    private func validate(_ timestamp: Date) -> Bool {
-        guard
-            let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheInDays, to: timestamp) // calendar dapet dari variable.
-        else { return false }
-        return currentDate() < maxCacheAge
     }
 }
 
@@ -71,7 +77,7 @@ extension LocalFeedLoader: FeedLoader {
             switch result {
                 /// case .found(feed: let localFeedImage, timestamp: let timestamp):
                 /// completion(.success(localFeedImage.toModels()))
-            case let .found(feed, timestamp) where validate(timestamp):
+            case let .found(feed, timestamp) where FeedCachePolicy.validate(timestamp, against: currentDate()):
                 completion(.success(feed.toModels()))
                 /*
                  case .found:
@@ -99,7 +105,7 @@ extension LocalFeedLoader {
                 store.deleteCachedFeed { _ in }
             
             /// Jika retrieve berhasil menemukan cache, tetapi timestamp-nya `tidak lagi` valid (sudah lebih dari 7 hari), hapus cache tersebut dari store.
-            case let .found(_, timestamp) where !self.validate(timestamp):
+            case let .found(_, timestamp) where !FeedCachePolicy.validate(timestamp, against: currentDate()):
                 store.deleteCachedFeed { _ in }
             
             /// kenapa tidak membuat `default`? ini untuk menjaga kualitas unit test dan mengingatkan kita jika sebuah case gagal akan kesini alih2 membuatnya default.
