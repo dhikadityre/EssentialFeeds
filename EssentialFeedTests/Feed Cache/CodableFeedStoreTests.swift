a2c99e80425f5b8b689f04b9d43cab52c0f0ee52
@algoreadme <dhikaaditya1@gmail.com>
2026-06-18 16:25:17 +0700

    feat: Move CodableFeedStore to the new infrastructure folder

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,114 +8,6 @@
 import XCTest
 import EssentialFeed
 
-class CodableFeedStore: FeedStore {
-    private struct Cache: Codable {
-        let feed: [CodableFeedImage]
-        let timestamp: Date
-        
-        var localFeed: [LocalFeedImage] {
-            return feed.map { $0.local }
-        }
-    }
-    
-    private struct CodableFeedImage: Codable {
-        private let id: UUID
-        private let description: String?
-        private let location: String?
-        private let url: URL
-        
-        init(_ localFeedImage: LocalFeedImage) {
-            self.id = localFeedImage.id
-            self.description = localFeedImage.description
-            self.location = localFeedImage.location
-            self.url = localFeedImage.url
-        }
-        
-        var local: LocalFeedImage {
-            LocalFeedImage(
-                id: id,
-                description: description,
-                location: location,
-                url: url
-            )
-        }
-    }
-    
-    private let storeURL: URL
-    private let queue = DispatchQueue(
-        label: "\(CodableFeedStore.self) Queue",
-        qos: .userInitiated,
-        attributes: .concurrent
-    )
-    
-    init(storeURL: URL) {
-        self.storeURL = storeURL
-    }
-    
-    func retrieve(completion: @escaping (RetrieveCompletion)) {
-        let storeURL = self.storeURL // Menyimpan copy of value storeURL
-        queue.async {
-            // guard let data = try? Data(contentsOf: self.storeURL) else {
-            guard let data = try? Data(contentsOf: storeURL) else { // Melakukan passing value `storeURL` alih2 reference `self`
-                return completion(.empty)
-            }
-            do {
-                let decoder = JSONDecoder()
-                let cache = try decoder.decode(Cache.self, from: data)
-                completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    func insert(
-        _ feed: [LocalFeedImage],
-        timestamp: Date,
-        completion: @escaping InsertionCompletion
-    ) {
-        /*
-         /// Menggunakan encoder dan menyelesaikan completion secara async
-        DispatchQueue.global().async {
-            let encoder = JSONEncoder()
-            let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-            let encoded = try! encoder.encode(cache)
-            try! encoded.write(to: self.storeURL)
-            completion(nil)
-        }
-        */
-         
-        queue.async(flags: .barrier) {
-            do {
-                let encoder = JSONEncoder()
-                let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-                let encoded = try encoder.encode(cache)
-                try encoded.write(to: self.storeURL)
-                completion(nil)
-            } catch {
-                completion(error)
-            }
-        }
-    }
-    
-    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
-        // completion(nil)
-        let storeURL = self.storeURL
-        queue.async(flags: .barrier) {
-            guard FileManager.default.fileExists(atPath: storeURL.path) else {
-                return completion(nil)
-            }
-            
-            do {
-                try FileManager.default.removeItem(at: storeURL)
-                completion(nil)
-            } catch {
-                completion(error)
-            }
-        }
-    }
-}
-
 typealias FailableFeedStoreSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
 
 class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
@@ -471,7 +363,7 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
         
         expect(sut, toRetrieve: .empty)
         */
-        assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
+        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     ///Expect running in order or serially
