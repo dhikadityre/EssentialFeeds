8b4ec18d3ad4cd3a94eeb20d4b2439285043c45c
@algoreadme <dhikaaditya1@gmail.com>
2026-06-18 15:15:57 +0700

    feat: CoreDataFeedStore.insert() delivers no error on empty cache

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -53,6 +53,7 @@ public final class CoreDataFeedStore: FeedStore {
                 let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
                 request.returnsObjectsAsFaults = false
                 if let cache = try context.fetch(request).first {
+                    /*
                     completion(.found(
                         feed: cache.feed
                             .compactMap { ($0 as? ManagedFeedImage) }
@@ -60,6 +61,8 @@ public final class CoreDataFeedStore: FeedStore {
                                 LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
                             },
                         timestamp: cache.timestamp))
+                    */
+                    completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
                 } else {
                     completion(.empty)
                 }
@@ -99,6 +102,7 @@ public final class CoreDataFeedStore: FeedStore {
             do {
                 let managedCache = ManagedCache(context: context)
                 managedCache.timestamp = timestamp
+                /*
                 managedCache.feed = NSOrderedSet(array: feed.map { local in
                     let managed = ManagedFeedImage(context: context)
                     managed.id = local.id
@@ -107,6 +111,8 @@ public final class CoreDataFeedStore: FeedStore {
                     managed.url = local.url
                     return managed
                 })
+                */
+                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
 
                 try context.save()
                 completion(nil)
@@ -160,6 +166,10 @@ private extension NSManagedObjectModel {
 private class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
+    
+    var localFeed: [LocalFeedImage] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
 }
 
 @objc(ManagedFeedImage)
@@ -169,4 +179,19 @@ private class ManagedFeedImage: NSManagedObject {
     @NSManaged var location: String?
     @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
+    
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            return managed
+        })
+    }
+    
+    var local: LocalFeedImage {
+        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
 }
