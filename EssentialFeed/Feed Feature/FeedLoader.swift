3e439afaffd41ba3909e919f00dc77d9bc9d8e3c
@algoreadme <dhikaaditya1@gmail.com>
2026-06-23 14:25:51 +0700

    feat: Replace custom LoadFeedResult enum with the standard Swift.Result

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -5,10 +5,13 @@
 import Foundation
 
 // contract
+/*
 public enum LoadFeedResult {
 	case success([FeedImage])
 	case failure(Error)
 }
+*/
+public typealias LoadFeedResult = Result<[FeedImage], Error>
 
 public protocol FeedLoader {
     func load(completion: @escaping (LoadFeedResult) -> Void)
