c5178dcfbe20d7ead63a3769811e098b68971bd6
@algoreadme <dhikaaditya1@gmail.com>
2026-06-25 15:40:13 +0700

    feat: Extract UIRefreshControl creation/configuration and refresh logic with `FeedLoader` to the new `FeedRefreshViewController`

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -23,34 +23,32 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
     
-    private var tableModel = [FeedImage]()
+    private var refreshController: FeedRefreshViewController?
+    
+    private var tableModel = [FeedImage]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     private var tasks = [IndexPath: FeedImageDataLoaderTask]()
     
     /// We use convenience init since we don't need any custom init. This way, we don't need to implement the UIViewController's required initialization
     public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
         self.init()
         self.feedLoader = feedLoader
+        self.refreshController = FeedRefreshViewController(feedLoader: feedLoader)
         self.imageLoader = imageLoader
     }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        tableView.prefetchDataSource = self
-        load()
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        feedLoader?.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.tableModel = feed
-                self?.tableView.reloadData()
-            }
-            self?.refreshControl?.endRefreshing()
+        refreshControl = refreshController?.view
+        refreshController?.onRefresh = { [weak self] feed in
+            self?.tableModel = feed
         }
+        tableView.prefetchDataSource = self
+        refreshController?.refresh()
     }
     
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
