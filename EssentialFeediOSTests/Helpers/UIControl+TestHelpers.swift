7478a4aac4ad08d3d26733f0fcc27dc71af2b55d
@algoreadme <dhikaaditya1@gmail.com>
2026-06-26 11:15:23 +0700

    feat: Extract duplicate logic into a reusable UIControl extension

---
+++ EssentialFeediOSTests/Helpers/UIControl+TestHelpers.swift
@@ -0,0 +1,18 @@
+//
+//  UIControl+TestHelpers.swift
+//  EssentialFeed
+//
+//  Created by DHIKA ADITYA ARE on 26/06/26.
+//
+
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
--- EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
+++ EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
@@ -7,12 +7,8 @@
 
 import UIKit
 
-private extension UIRefreshControl {
+extension UIRefreshControl {
     func simulatePullToRefresh() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
+        simulate(event: .valueChanged)
     }
 }
