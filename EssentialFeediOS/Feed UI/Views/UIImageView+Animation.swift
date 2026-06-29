a2ad4596cec233c66e51373dddf9bff80a967f8e
@algoreadme <dhikaaditya1@gmail.com>
2026-06-29 15:33:30 +0700

    feat: Display image with animation

---
+++ EssentialFeediOS/Feed UI/Views/UIImageView+Animation.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageView+Animation.swift
+//  EssentialFeed
+//
+//  Created by DHIKA ADITYA ARE on 29/06/26.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
