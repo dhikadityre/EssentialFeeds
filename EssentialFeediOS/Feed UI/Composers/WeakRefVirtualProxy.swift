1d14596c075365164c0b4b111cd3b29bf07e91e6
@algoreadme <dhikaaditya1@gmail.com>
2026-06-30 13:35:54 +0700

    feat: Move FeedViewAdapter to separate file and Move WeakRefVirtualProxy to separate file

---
+++ EssentialFeediOS/Feed UI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,32 @@
+//
+//  WeakRefVirtualProxy.swift
+//  EssentialFeed
+//
+//  Created by DHIKA ADITYA ARE on 30/06/26.
+//
+
+import UIKit
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(viewModel: FeedLoadingViewModel) {
+        object?.display(
+            viewModel: FeedLoadingViewModel(
+                isLoading: viewModel.isLoading
+            )
+        )
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModels<UIImage>) {
+        object?.display(model)
+    }
+}
