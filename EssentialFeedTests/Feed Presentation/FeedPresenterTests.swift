e320f27b0ac369473ee841d3c1ede7cd47a868b1
@algoreadme <dhikaaditya1@gmail.com>
2026-07-01 10:45:22 +0700

    feat: Extract system under test creation to a test factory method

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -17,7 +17,7 @@ class FeedPresenterTests: XCTestCase {
     /// Direkomendasikan untuk memulai dari `Degenerate`, `Behavior Trivial`, dan Simple.
     /// Di case ini, kita mensimplekan bahwa `FeedPresenter` tidak menjalankan apapun di `constructor`
     func test_init_doesNotSendMessageToView() {
-        let view = ViewSpy()
+        let (_, view) = makeSUT()
         
         _ = FeedPresenter(view: view)
         
@@ -25,6 +25,17 @@ class FeedPresenterTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT(
+        file: StaticString = #file, line: UInt = #line
+    ) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
+    
     private class ViewSpy {
         let messages = [Any]()
     }
