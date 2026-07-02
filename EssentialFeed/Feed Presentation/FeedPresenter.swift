77f370c3a445d5dd5206a47abf667775a10a0db8
@algoreadme <dhikaaditya1@gmail.com>
2026-07-02 15:08:58 +0700

    feat: Move prod types to the EssentialFeed production module

---
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -0,0 +1,88 @@
+//
+//  FeedPresenter.swift
+//  EssentialFeed
+//
+//  Created by DHIKA ADITYA ARE on 02/07/26.
+//
+
+import Foundation
+
+public struct FeedViewModels {
+    public let feed: [FeedImage]
+}
+
+public protocol FeedView {
+    func display(viewModel: FeedViewModels)
+}
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    public static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    public static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
+
+public protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
+
+public protocol FeedLoadingView {
+    func display(viewModel: FeedLoadingViewModel)
+}
+
+public final class FeedPresenter {
+    private let feedView: FeedView
+    private let loadingView: FeedLoadingView
+    private let errorView: FeedErrorView
+    
+    public init(
+        feedView: FeedView,
+        loadingView: FeedLoadingView,
+        errorView: FeedErrorView
+    ) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+    
+    public static var title: String {
+        return NSLocalizedString(
+            "FEED_VIEW_TITLE",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Title for the feed view"
+        )
+    }
+    
+    private var feedLoadError: String {
+        return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Error message displayed when we can't load the image feed from the server")
+    }
+
+    
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: true))
+    }
+    
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(viewModel: FeedViewModels(feed: feed))
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: false))
+    }
+    
+    public func didFinishLoadingFeed(with error: Error) {
+        errorView.display(.error(message: feedLoadError))
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: false))
+    }
+}
