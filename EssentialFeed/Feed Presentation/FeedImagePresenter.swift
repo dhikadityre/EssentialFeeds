a9a76ffbeab4adaa72138d979619a06c3d18b25b
@algoreadme <dhikaaditya1@gmail.com>
2026-07-02 20:12:55 +0700

    feat: Move `FeedImagePresenter` and collaborators to production

---
+++ EssentialFeed/Feed Presentation/FeedImagePresenter.swift
@@ -0,0 +1,76 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeed
+//
+//  Created by DHIKA ADITYA ARE on 02/07/26.
+//
+
+import Foundation
+
+public class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private let imageTransformer: (Data) -> Image?
+
+    public init(
+        view: View,
+        imageTransformer: @escaping (Data) -> Image?
+    ) {
+        self.view = view
+        self.imageTransformer = imageTransformer
+    }
+    
+    public func didStartLoadingImageData(for model: FeedImage) {
+        view.display(
+            FeedImageViewModels(
+                description: model.description,
+                location: model.location,
+                image: nil,
+                isLoading: true,
+                shouldRetry: false
+            )
+        )
+    }
+    
+    public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        let image = imageTransformer(data)
+        view.display(
+            FeedImageViewModels(
+                description: model.description,
+                location: model.location,
+                image: image,
+                isLoading: false,
+                shouldRetry: image == nil
+            )
+        )
+    }
+    
+    public func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+        view.display(
+            FeedImageViewModels(
+                description: model.description,
+                location: model.location,
+                image: nil,
+                isLoading: false,
+                shouldRetry: true
+            )
+        )
+    }
+}
+
+public protocol FeedImageView {
+    associatedtype Image
+    
+    func display(_ model: FeedImageViewModels<Image>)
+}
+
+public struct FeedImageViewModels<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
