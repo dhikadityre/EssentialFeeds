484ced9fb292a67e04ca30e1ea24d0f8d1d82a55
@algoreadme <dhikaaditya1@gmail.com>
2026-07-02 22:23:57 +0700

    feat: `RemoteFeedImageDataLoader` requests data from URL twice on calling `loadImageDataFromURL` twice

--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -41,6 +41,16 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
+    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.loadImageData(from: url, completion: { _ in })
+        sut.loadImageData(from: url, completion: { _ in })
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helper
     private func makeSUT(
         url: URL = anyURL(),
