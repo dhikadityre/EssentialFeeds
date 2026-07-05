e3807264fe0950e37ea413621f472eff65eca4eb
@algoreadme <dhikaaditya1@gmail.com>
2026-07-05 21:38:37 +0700

    feat: `LocalFeedLoader.validateCache` succeeds on empty cache

--- EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -112,6 +112,14 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_validateCache_succeedsOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeRetrieveWithEmptyCache()
+        })
+    }
+    
     func test_validateCache_doesNotDeleteCacheAfterSUTInstanceHasBeenDealocated() {
         let store = FeedStoreSpy()
         var localFeedLoader: LocalFeedLoader? = LocalFeedLoader(
